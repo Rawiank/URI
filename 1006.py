979c595b76a00725d0cc4399f8a71f6701757bf7
rawiank <victor.alvesf7@gmail.com>
2017-06-28 11:51:05 -0400

    Solução para o exercício 1006

---
+++ 1006.py
@@ -0,0 +1,10 @@
+a = float(input(""))
+b = float(input(""))
+c = float(input(""))
+
+if a >=0 <=10 and b >=0 <=10:
+    a = a * (2)
+    b = b * (3)
+    c = c * (5)
+    media = (a + b + c) / 10.0
+    print "MEDIA = %.1f" %media
