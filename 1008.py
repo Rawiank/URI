fe6b9a34a63ea42076253c18796dc081df6051ba
rawiank <victor.alvesf7@gmail.com>
2017-06-29 20:57:39 -0400

    Solução para o exercício 1008

---
+++ 1008.py
@@ -0,0 +1,6 @@
+numeroFuncionario = int(input(""))
+horasTrabalhadas = int(input(""))
+valorPorHora = float(input(""))
+salario = horasTrabalhadas * valorPorHora
+
+print 'NUMBER = %d' %numeroFuncionario + '\nSALARY = U$ %.2f' %salario
